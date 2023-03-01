2ed065c51676971786328caa2256b7617ac67bd2
Rachel Daniel <racheldaniel328@gmail.com>
2023-03-01 12:33:05 -0700

    added styles

--- Mission09_red328/Infrastructure/PaginationTagHelper.cs
+++ Mission09_red328/Infrastructure/PaginationTagHelper.cs
@@ -27,6 +27,12 @@ namespace Mission09_red328.Infrastructure
         
         public PageInfo PageBlah { get; set; }
         public string PageAction { get; set; }
+        
+        // add these classes for CSS
+        public bool PageClassesEnabled { get; set; } = false;
+        public string PageClass { get; set; }
+        public string PageClassNormal { get; set; }
+        public string PageClassSelected { get; set; }
 
         public override void Process (TagHelperContext thc, TagHelperOutput tho)
         {
@@ -38,7 +44,13 @@ namespace Mission09_red328.Infrastructure
             {
                 TagBuilder tb = new TagBuilder("a");
                 tb.Attributes["href"] = uh.Action(PageAction, new { pageNum = i });
-                tb.InnerHtml.Append("Page " + i.ToString());
+                if (PageClassesEnabled)
+                {
+                    tb.AddCssClass(PageClass);
+                    tb.AddCssClass(i == PageBlah.CurrentPage
+                        ? PageClassSelected : PageClassNormal);
+                }
+                tb.InnerHtml.Append(i.ToString());
 
                 final.InnerHtml.AppendHtml(tb);
             }
