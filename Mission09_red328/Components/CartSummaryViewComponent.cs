179a9c4af79bb7f014d5cb8d23354db52bb7e9e3
Rachel Daniel <racheldaniel328@gmail.com>
2023-03-13 13:39:08 -0600

    added cart

---
+++ Mission09_red328/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission09_red328.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_red328.Views.Shared.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Cart cart;
+
+        public CartSummaryViewComponent (Cart cartService)
+        {
+            cart = cartService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+    }
+}
